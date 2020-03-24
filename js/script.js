1fedc160054793a9d7e84c1fc6c86c649c26e9ba
Nikanora <37082879+Nikanorra@users.noreply.github.com>
2020-03-24 11:25:55 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ $ (document).ready(function() {
 	$('#category_selec, #selects1').selectmenu();
 
 });
+
+let popup = document.getElementById('popup_enter'),
+		popupToggle = document.getElementById('myBtn'),
+		popupClose = document.querySelector('.close');
+
+		popupToggle.onclick = function() {
+			popup.style.display="block";
+		};
+
+		popupClose.onclick = function() {
+			popup.style.display="none";
+		}
+
+		window.onclick = function(e) {
+			if(e.target == popup) {
+				popup.style.display="none";
+			}
+		}
